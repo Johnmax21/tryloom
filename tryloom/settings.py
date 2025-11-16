6120efc61529e8bb9e688a7d7c99a868e168db12
JOHN <johnthomas.lm10@gmail.com>
2025-11-16 18:21:48 +0530

    Configured WhiteNoise static files

--- tryloom/settings.py
+++ tryloom/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,12 +138,13 @@ EMAIL_HOST_PASSWORD = 'lsjz ksqq cjhz qkca'
 EMAIL_USE_TLS = True 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 RAZOR_KEY_ID ='rzp_live_RYNqw9fDePuWLu'
